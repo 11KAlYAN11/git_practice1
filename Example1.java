38f3b9454853d1372315da3482009ac553008a4e
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-11-09 07:05:05 +0000

    added for loop

--- Example1.java
+++ Example1.java
@@ -5,5 +5,9 @@ public static void main(String[] args) {
     System.out.println("hello");
     System.out.println("World");
     System.out.println("amaran");
+    for(int i=0;i<=10;i++)
+    {
+        System.out.println(i);
+    }
 }
 }
