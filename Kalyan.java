d2b5a20a0ab5e9cd8d9aa0e7a99f54a0c51d6f5b
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-11-09 14:23:07 +0000

    .........Removed Everything......

--- Kalyan.java
+++ Kalyan.java
@@ -1,18 +1,5 @@
 public class Kalyan {
     public static void main(String[] args) {
-        System.out.println("change1");
-        System.out.println("change2");
-        System.out.println("change3");
-        System.out.println("change4");
-        System.out.println("change5");
-        System.out.println("change6");
-        System.err.println("change7");
-        System.out.println("change8");
-        System.out.println("change9");
-        System.err.println("change10");
-        System.out.println("Hello");
-        System.out.println("World");
-        System.out.println("Welcome!");
-        System.out.println("Let/'s see");
+        
     }
 }
