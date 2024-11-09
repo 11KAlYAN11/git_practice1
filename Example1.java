c0956c1e51ac9e2516bf92755feddde7a8ab17c1
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-11-09 07:10:25 +0000

    removed amaran

--- Example1.java
+++ Example1.java
@@ -4,7 +4,6 @@ public static void main(String[] args) {
 
     System.out.println("hello");
     System.out.println("World");
-    System.out.println("amaran");
     for(int i=0;i<=10;i++)
     {
         System.out.println(i);
