60139f117d6bf16fc07584bc40ba174266dbda1c
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-11-09 06:45:53 +0000

    error fixed

--- Example1.java
+++ Example1.java
@@ -1,6 +1,8 @@
 public class Example1 {
 public static void main(String[] args) {
     System.out.println("Hello example from main");
-    sss
+
+    System.out.println("hello");
+    System.out.println("World");
 }
 }
