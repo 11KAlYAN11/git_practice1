b782ff6ef58b12a6eeb0270416f91160f5ccf783
Asam Pavan Kumar Reddy <76771308+11KAlYAN11@users.noreply.github.com>
2024-11-09 09:17:50 +0000

    s

--- Demo1.java
+++ Demo1.java
@@ -5,6 +5,6 @@ public class Demo1
         System.out.println("Credit cards");
         System.out.println("Products");
         System.out.println("xyz");
-        System.out.println("hello siva!");
+        System.out.println("hello siva!"); 
     }
 }
